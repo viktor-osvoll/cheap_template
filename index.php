e1c60c01b6d4a615501ba79ac09825d2f9172b77
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-09-09 13:24:50 +0200

    scroll-down-menu

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
 $context['fields'] = get_fields(8);
-$context['abouts'] = get_fields(12);
-$context['tests'] = new Timber\Post();
+/* $context['abouts'] = get_fields(12);
+$context['tests'] = new Timber\Post(); */
 $templates = array( 'index.twig' );
 
 if ( is_home() ) {
